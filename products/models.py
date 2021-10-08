8a9de8b4d0bb30bb8bde28d44eede6219e223178
OniAkuma96 <harrison.tudball@hotmail.com>
2021-10-08 16:36:33 +0000

    added product model and admin

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    main_genre = models.CharField(max_length=254)
+    sub_genre = models.CharField(max_length=254, null=True, blank=True)
+    title = models.CharField(max_length=254)
+    author = models.CharField(max_length=254)
+    synopsis = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
