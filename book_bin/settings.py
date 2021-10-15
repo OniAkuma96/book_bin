9c00c1f4e13f79bc7b0f9cc27d57b8e3db433d4e
OniAkuma96 <harrison.tudball@hotmail.com>
2021-10-15 17:01:38 +0000

    added checkout success logic

--- book_bin/settings.py
+++ book_bin/settings.py
@@ -173,7 +173,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-DELIVERY_PERCENTAGE = 10
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Stripe
 STRIPE_CURRENCY = 'gbp'
