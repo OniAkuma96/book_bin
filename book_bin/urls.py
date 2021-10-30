53c6524aad6256722b84b153106667a5cd062459
OniAkuma96 <harrison.tudball@hotmail.com>
2021-10-30 17:25:58 +0000

    removed reviews url form project level urls file too

--- book_bin/urls.py
+++ book_bin/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
-    path('reviews/', include('reviews.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
