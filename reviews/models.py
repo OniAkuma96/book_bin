cd580c5f6775a82a86cb526e35561769de7b9be4
OniAkuma96 <harrison.tudball@hotmail.com>
2021-10-30 17:41:32 +0000

    updated reviews model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from products.models import Product
+from profiles.models import UserProfile
 
+
+class Review(models.Model):
+    created_by = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True)
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField()
+    rating = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.created_by
