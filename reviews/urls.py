89cb2160ce4fe400c6add179c93707d9bee3e31c
OniAkuma96 <harrison.tudball@hotmail.com>
2021-11-01 15:12:36 +0000

    setup review urls and template, beginning review form

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('new_review/', views.new_review, name='new_review'),
+    path('new_review/<product_id>/', views.new_review, name='new_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
 
-def new_review(request):
-    return render(request, 'reviews/new_review.html')
+def new_review(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'reviews/new_review.html', context)
