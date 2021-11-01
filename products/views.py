4af9198bbb61fe44b07989344e90945cfddc88be
OniAkuma96 <harrison.tudball@hotmail.com>
2021-11-01 13:58:41 +0000

    removed if not query line form product views

--- products/views.py
+++ products/views.py
@@ -20,9 +20,9 @@ def all_products(request):
 
         if 'q' in request.GET:
             query = request.GET['q']
-            if not query:
-                messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('products'))
+            # if not query:
+            #     messages.error(request, "You didn't enter any search criteria!")
+            #     return redirect(reverse('products'))
         
             queries = (
                 Q(title__icontains=query) | Q(author__icontains=query) | Q(sub_genre__icontains=query)
