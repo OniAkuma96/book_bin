4476a7a9732e5bc37ec754641920d88f21d91388
OniAkuma96 <harrison.tudball@hotmail.com>
2021-11-02 17:10:14 +0000

    added custom widget for image on edit product form

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product
 
 
@@ -8,8 +9,10 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
-            for field_name, field in self.fields.items():
-                field.widget.attrs['class'] = 'border-black rounded-0'
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
